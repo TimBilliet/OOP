c3a4a22b6b9aeea8bc2782e427268397561a758a
Tim Billiet <tim.billiet@student.odisee.be>
2024-10-11 13:57:31 +0200

    oef6 af

---
+++ Reeks03/Bewerkingen/src/MainOef6.java
@@ -0,0 +1,8 @@
+import java.io.FileNotFoundException;
+
+public class MainOef6 {
+    public static void main(String[] args) throws FileNotFoundException {
+        new Oefeningenreeks("moeilijker.txt");
+
+    }
+}
--- Reeks03/Bewerkingen/src/Oefeningenreeks.java
+++ Reeks03/Bewerkingen/src/Oefeningenreeks.java
@@ -1,9 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Oefeningenreeks {
+    private int aantalOefeningen;
+    private int aantalJuisteAntwoorden;
 
-    public Oefeningenreeks() {
-
+    public Oefeningenreeks(String bestandsnaam) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(bestandsnaam));
+        while (scanner.hasNext()) {
+            String input = scanner.nextLine();
+            if(input.isEmpty()){
+                continue;
+            }
+            geefOefeningAanGebruiker(new Bewerking(input));
+            aantalOefeningen++;
+        }
+        System.out.println(this);
     }
-    public void geefOefeningAanGebruiker() {
 
+    public void geefOefeningAanGebruiker(Bewerking bewerking) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(bewerking);
+        String input = scanner.nextLine().trim();
+        if (Integer.parseInt(input) == bewerking.oplossing()) {
+            System.out.println(":)");
+            aantalJuisteAntwoorden++;
+        } else {
+            System.out.println(":(");
+        }
+    }
+
+    @Override
+    public String toString() {
+        String feedback = "U heeft " + aantalJuisteAntwoorden + " van de " + aantalOefeningen + " oefeningen juist opgelost.";
+
+        if (aantalJuisteAntwoorden <= aantalOefeningen / 2) {
+            feedback += " Niet opgeven, blijven oefenen!!";
+        }
+
+        return feedback;
     }
 }
