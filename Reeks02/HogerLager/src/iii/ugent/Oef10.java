bc3a3472f4c9480e20ce586f759b62eb4a0f6f7e
tim.billiet <tim.billiet@student.odisee.be>
2024-10-15 22:04:06 +0200

    verbetering hogerlager

--- Reeks02/HogerLager/src/iii/ugent/Oef10.java
+++ Reeks02/HogerLager/src/iii/ugent/Oef10.java
@@ -10,8 +10,30 @@ public class Oef10 {
         System.out.println("Hoger of lager?");
         Scanner scanner = new Scanner(System.in);
         Kaart nieuweKaart = BoekKaarten.geefKaart();
-        boolean actief = true;
-        while (scanner.hasNext() && actief) {
+        boolean spelActief = true;
+
+        while (spelActief) {
+            String input = scanner.nextLine().trim().toLowerCase();
+            while((!input.equals("hoger")) && (!input.equals("lager"))) {
+                System.out.println("input was: " + input);
+                System.out.println("Antwoord met 'hoger' of 'lager' aub");
+                input = scanner.nextLine().trim().toLowerCase();
+            }
+            if (input.equals("hoger") && nieuweKaart.isHogerDan(huidigeKaart) || input.equals("lager") && nieuweKaart.isLagerDan(huidigeKaart)) {
+                System.out.println("JUIST");
+                System.out.println("De volgende kaart was " + nieuweKaart + " (waarde: " + nieuweKaart.getWaarde() + ")");
+                System.out.println("Hoger of lager?");
+                huidigeKaart = nieuweKaart;
+                nieuweKaart = BoekKaarten.geefKaart();
+            } else if (input.equals("lager") && nieuweKaart.isHogerDan(huidigeKaart) || input.equals("hoger") && nieuweKaart.isLagerDan(huidigeKaart)
+                    || nieuweKaart.isGelijkAan(huidigeKaart)) {
+                System.out.println("FOUT! Jammer, de volgende kaart was " + nieuweKaart);
+                spelActief = false;
+            }
+        }
+
+        /*
+        while (spelActief && scanner.hasNext()) {
             String input = scanner.nextLine().trim().toLowerCase();
             if (input.equals("hoger") && nieuweKaart.isHogerDan(huidigeKaart) || input.equals("lager") && nieuweKaart.isLagerDan(huidigeKaart)) {
                 System.out.println("JUIST");
@@ -22,10 +44,11 @@ public class Oef10 {
             } else if (input.equals("lager") && nieuweKaart.isHogerDan(huidigeKaart) || input.equals("hoger") && nieuweKaart.isLagerDan(huidigeKaart)
                     || (input.equals("lager") || input.equals("hoger")) && nieuweKaart.isGelijkAan(huidigeKaart)) {
                 System.out.println("FOUT! Jammer, de volgende kaart was " + nieuweKaart);
-                actief = false;
+                 spelActief = false;
             } else {
                 System.out.println("Antwoord met 'hoger' of 'lager' aub");
             }
         }
+         */
     }
 }
