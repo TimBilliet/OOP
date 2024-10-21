8ce24e4e26ca11f38cd8b9d30963e488363ac780
Tim Billiet <tim.billiet@student.odisee.be>
2024-10-21 13:26:41 +0200

    toevoeging woordzoeker bestanden

---
+++ Reeks04/Woordenzoeker/src/woordenzoeker/Main.java
@@ -0,0 +1,16 @@
+package woordenzoeker;
+
+/**
+ *
+ * @author tiwi
+ */
+public class Main {
+
+    public static void main(String[] args)  {
+        
+        String[] bestandsnamen = {"locatieSPATIE.txt","locatieKOMMAPUNT.txt","locatieKOMMA.txt","locatieDUBBELEPUNT.txt"};
+        
+        // vul aan
+    }
+    
+}
