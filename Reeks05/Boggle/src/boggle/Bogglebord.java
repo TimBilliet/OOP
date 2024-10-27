2caee13c00c101a8506966a9fcb2764a898966bc
tim.billiet <tim.billiet@student.odisee.be>
2024-10-27 12:30:16 +0100

    oef7 boggle schud

--- Reeks05/Boggle/src/boggle/Bogglebord.java
+++ Reeks05/Boggle/src/boggle/Bogglebord.java
@@ -1,37 +1,54 @@
 package boggle;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
- *
  * @author ...
  */
 public class Bogglebord {
 
     public static final int DIM = 4;
-    
-    private Dobbelsteen[][] bord;
 
-    public Bogglebord () {
-        String[] letters = {"NTOKEZ", "CDEPAM", "LUNPET", "MOARSI", "DONSTE", "HIEFES", "RESHCA", "BQMJOD",
-                "TIALNB", "GYNJEU", "ZAEDNV", "AOIEAW", "HSIREN", "GENVIT", "WEGLUR", "RIAFXK"};
+    private Dobbelsteen[][] bord;
+    private String[] letters = {"AAAAAA", "BBBBBB", "CCCCCC", "DDDDDD", "EEEEEE", "FFFFFF", "GGGGGG", "HHHHHH",
+            "IIIIII", "JJJJJJ", "KKKKKK", "LLLLLL", "MMMMMM", "NNNNNN", "OOOOOO", "PPPPPP"};
+
+    public Bogglebord() {
+        //String[] letters = {"NTOKEZ", "CDEPAM", "LUNPET", "MOARSI", "DONSTE", "HIEFES", "RESHCA", "BQMJOD", "TIALNB", "GYNJEU", "ZAEDNV", "AOIEAW", "HSIREN", "GENVIT", "WEGLUR", "RIAFXK"};
         bord = new Dobbelsteen[DIM][DIM];
-        for(int i = 0; i < DIM; i++) {
-            for(int j = 0; j < DIM; j++) {
-                bord[i][j] = new Dobbelsteen(letters[i*DIM+j]);
+        for (int i = 0; i < DIM; i++) {
+            for (int j = 0; j < DIM; j++) {
+                bord[i][j] = new Dobbelsteen(letters[i * DIM + j]);
             }
         }
     }
+
     public void dobbel() {
-        for(int i = 0; i < DIM; i++) {
-            for(int j = 0; j < DIM; j++) {
+        for (int i = 0; i < DIM; i++) {
+            for (int j = 0; j < DIM; j++) {
                 bord[i][j].dobbel();
             }
         }
     }
 
+    public void schud() {
+        Integer[] temparray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        List<Integer> lijstvanarray = Arrays.asList(temparray);
+        Collections.shuffle(lijstvanarray);
+        String[] newStringArray  = new String[16];
+        for(int i = 0; i < newStringArray.length; i++){
+            newStringArray[i] = letters[temparray[i]];
+        }
+        letters = newStringArray;
+        dobbel();
+    }
+
     public String toString() {
         String result = "";
-        for(int i = 0; i < DIM; i++) {
-            for(int j = 0; j < DIM; j++) {
+        for (int i = 0; i < DIM; i++) {
+            for (int j = 0; j < DIM; j++) {
                 System.out.print(bord[i][j].getWaarde() + " ");
             }
             System.out.println();
--- Reeks05/Boggle/src/boggle/Main.java
+++ Reeks05/Boggle/src/boggle/Main.java
@@ -8,10 +8,10 @@ public class Main {
 
     public static void main(String[] args) {
         Bogglebord bord1 = new Bogglebord();
-
-        System.out.println(bord1);
-        bord1.dobbel();
-        System.out.println(bord1);
+            bord1.schud();
+//        System.out.println(bord1);
+//        bord1.dobbel();
+//        System.out.println(bord1);
         // to do
     }
     
