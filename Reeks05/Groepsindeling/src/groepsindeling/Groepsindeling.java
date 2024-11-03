9cb96eb12a467e5902988a3c02be0dcad0a540fc
tim.billiet <tim.billiet@student.odisee.be>
2024-11-03 19:35:40 +0100

    oef8 groepsindeling werkend

--- Reeks05/Groepsindeling/src/groepsindeling/Groepsindeling.java
+++ Reeks05/Groepsindeling/src/groepsindeling/Groepsindeling.java
@@ -4,35 +4,59 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-/**
- *
- * @author tiwi
- */
+
 public class Groepsindeling {
-    private Map<Integer, Set<Persoon>> personenPerErvaring = new HashMap<>();
+    private Map<Integer, Set<Persoon>> personenPerErvaring = new TreeMap<>();
+
     public Groepsindeling(String bestandsnaam) throws FileNotFoundException {
         Scanner sc = new Scanner(new File(bestandsnaam));
-        while(sc.hasNext()) {
+        Map<Integer, List<Persoon>> tempMap = new TreeMap<>();
+        while (sc.hasNext()) {
             String[] lijn = sc.next().split(";");
             int ervaring = Integer.parseInt(lijn[1]);
             String naam = lijn[0];
             Persoon toeTeVoegenPersoon = new Persoon(naam, ervaring);
-            if(personenPerErvaring.containsKey(ervaring)) {
+            if (personenPerErvaring.containsKey(ervaring)) {
                 personenPerErvaring.get(ervaring).add(toeTeVoegenPersoon);
             } else {
-                personenPerErvaring.put(ervaring, new HashSet<>(){{add(toeTeVoegenPersoon);}});
+                personenPerErvaring.put(ervaring, new HashSet<>() {{
+                    add(toeTeVoegenPersoon);
+                }});
             }
         }
     }
 
     public Map<Integer, Set<Persoon>> groepenMetGelijkeErvaring(int aantalLeden) {
-    return null;
+        Map<Integer, Set<Persoon>> groepen = new TreeMap<>();
+        List<Persoon> samengevoegdeLijst = new ArrayList<>();
+
+        for (Map.Entry<Integer, Set<Persoon>> entry : personenPerErvaring.entrySet()) {
+            List<Persoon> shuffledLijst = new ArrayList<>(entry.getValue());
+            Collections.shuffle(shuffledLijst);
+            samengevoegdeLijst.addAll(shuffledLijst);
+        }
+        int aantalgroepen = samengevoegdeLijst.size() / aantalLeden;
+        int aantalGroteGroepen = samengevoegdeLijst.size() % aantalLeden;
+        int groteGroepenCounter = 0;
+
+        for(int i = 0; i < aantalgroepen; i++) {
+            if(i >= aantalgroepen - aantalGroteGroepen) {
+                groteGroepenCounter++;
+                if(groteGroepenCounter > 1) {
+                    groepen.put(i + 1, new HashSet<>(samengevoegdeLijst.subList((i * aantalLeden) + 1, ((i + 1) * aantalLeden) + 2 )));
+                } else {
+                    groepen.put(i + 1, new HashSet<>(samengevoegdeLijst.subList(i * (aantalLeden), ((i + 1) * aantalLeden) + 1 )));
+                }
+            } else {
+                groepen.put(i + 1, new HashSet<>(samengevoegdeLijst.subList(i * aantalLeden, (i + 1) * aantalLeden)));
+            }
+        }
+        return groepen;
     }
 
     public Map<Integer, Set<Persoon>> groepenMetGespreideErvaring(int aantalLeden) {
-return null;
+        return null;
     }
-    
-    
-    
+
+
 }
--- Reeks05/Groepsindeling/src/groepsindeling/Main.java
+++ Reeks05/Groepsindeling/src/groepsindeling/Main.java
@@ -13,11 +13,11 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Groepsindeling groepsindeling = new Groepsindeling("personen.csv");
         
-        Map<Integer,Set<Persoon>> groepenGelijk = groepsindeling.groepenMetGelijkeErvaring(4);
+        Map<Integer,Set<Persoon>> groepenGelijk = groepsindeling.groepenMetGelijkeErvaring(6);
         Map<Integer,Set<Persoon>> groepenGespreid = groepsindeling.groepenMetGespreideErvaring(4);
         
         System.out.println(groepenGelijk.toString());
-        System.out.println(groepenGespreid.toString());                
+        //System.out.println(groepenGespreid.toString());
     }
     
 }
