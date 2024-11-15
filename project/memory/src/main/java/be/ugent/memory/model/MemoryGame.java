88c6460b5c5e8ee501550436769c8221fe961e84
tim.billiet <tim.billiet@student.odisee.be>
2024-11-15 18:51:27 +0100

    fix

--- project/memory/src/main/java/be/ugent/memory/model/MemoryGame.java
+++ project/memory/src/main/java/be/ugent/memory/model/MemoryGame.java
@@ -3,6 +3,8 @@ package be.ugent.memory.model;
 import be.ugent.memory.MemoryView;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class MemoryGame implements IMemoryGame {
 
@@ -10,10 +12,16 @@ public class MemoryGame implements IMemoryGame {
     private int kolommen;
     private Kaart[][] bord;
     private Kaart previousCard;
-    private int moves;
-    private int flippedCount;
+    private int aantalZetten;
+    private int aantalGevondenDuos;
 
     public MemoryGame(File bestand) {
+        try(Scanner sc = new Scanner(bestand)) {
+            rijen = sc.nextInt();
+            kolommen  = sc.nextInt();
+        } catch (FileNotFoundException ex) {
+            System.out.println("Bestand niet gevonden");
+        }
     }
 
     public MemoryGame(int aantalRijen, int aantalKolommen) {
@@ -56,22 +64,30 @@ public class MemoryGame implements IMemoryGame {
 
     @Override
     public void selectCard(int row, int col) {
-        if (moves != 0 && bord[row][col].getValue().equals(previousCard.getValue())) {
-            previousCard.setFlipped(true);
+        if(!bord[row][col].isFlipped()) { // draait de kaart alleen om als die nog niet omgedraaid is
             bord[row][col].setFlipped(true);
-            flippedCount++;
+            aantalZetten++;
+            //volgorde van de if else belangrijk
+
+            if(previousCard != null && bord[row][col].getValue().equals(previousCard.getValue())) { // als de waarde van de geselecteerde kaart gelijk is met de vorige kaart is er een duo gevonden
+                aantalGevondenDuos ++;
+            } else if(aantalZetten % 2 == 0 && aantalZetten > 1){ // als de 2de kaart van een poging wordt gedraait en deze niet gelijk waren(in de if ervoor gecheckt), draai beide kaarten weer om
+                bord[row][col].setFlipped(false);
+                previousCard.setFlipped(false);
+                previousCard = null;
+                return;
+            }
+            previousCard = bord[row][col]; // maak de vorige kaart de kaart die net geselecteerd werd
         }
-        previousCard = bord[row][col];
-        moves++;
     }
 
     @Override
     public boolean isGameOver() {
-        return flippedCount == rijen * kolommen / 2;
+        return aantalGevondenDuos == rijen * kolommen / 2;
     }
 
     @Override
     public int getMoveCount() {
-        return moves;
+        return aantalZetten;
     }
 }
