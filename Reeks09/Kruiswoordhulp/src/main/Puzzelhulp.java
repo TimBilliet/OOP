2d86d255c87e45b0d1cc4fb201cb398bf7713d06
Tim Billiet <tim.billiet@student.odisee.be>
2024-12-13 11:13:47 +0100

    reeks 9 startproj

---
+++ Reeks09/Kruiswoordhulp/src/main/Puzzelhulp.java
@@ -0,0 +1,40 @@
+package main;
+
+import java.util.*;
+
+public class Puzzelhulp {
+
+    private List<String> woorden;
+
+    public Puzzelhulp(String bestandsnaamWoordenlijst) {
+
+    }
+
+    public int aantalWoordenMetLengte(int lengte) {
+        return 0;
+    }
+
+    public List<String> woordenMetLengte(int lengte) {
+        return new ArrayList<String>();
+    }
+
+    public List<String> woordenMetMeerKlinkersDanMedeklinkers() {
+        return new ArrayList<String>();
+    }
+
+
+    public void schrijfWoordenMetLetters(char... letters) {
+        System.out.println("niet geïmplementeerd");
+    }
+
+    public String alfabetischEerste(int lengte) {
+        return "-";
+    }
+
+    public String alfabetischLaatste(int lengte) {
+        return "-";
+    }
+
+
+}
+
